3c920f3d8a7ac6e734b5149846d4d076159b118d
swadipa123 <40620169+swadipa123@users.noreply.github.com>
2020-09-17 22:56:32 +0530

    splash screen

--- app/src/main/java/com/example/splashscreen_logo/MainActivity.java
+++ app/src/main/java/com/example/splashscreen_logo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.splashscreen_logo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,activity_splash.class);
+                startActivity(intent);
+                finish();  //distroys activity object (when click on back then open home screen not open splashscreen )
+
+            }
+        },2500);// after 2.5 sec next activity open
     }
 }
